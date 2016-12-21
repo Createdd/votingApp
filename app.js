828efa75386b4afa1218c0079f0ebfbae12bd955
Daniel Deutsch <deudan1010@gmail.com>
2016-12-21 17:42:26 +0100

    update and add mongoose connection

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+
 'use strict';
 
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://test:test@ds141368.mlab.com:41368/votingapp');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error'));
+db.once('open', () => {
+  console.log('Connection to MongoDB successful!');
+});
 app.use('/', express.static('public'));
 
 app.listen(port, function(){
