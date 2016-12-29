d96e623358ade168aefaf534781b24c177572921
Daniel Deutsch <deudan1010@gmail.com>
2016-12-29 22:56:15 +0100

    add poll validation

--- app/models/poll.js
+++ app/models/poll.js
@@ -2,6 +2,24 @@
 const mongoose = require('mongoose');
 const validate = require('mongoose-validator');
 
+//----------Validations
+let questionValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [6,250],
+    message: 'Question should have between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+let choicesValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [2,30],
+    message: 'The Range of Choices is between {ARGS[0]} and {ARGS[1]}'
+  })
+];
+
+
+//----------User Schema
 var pollSchema = mongoose.Schema({
   question: {
     type: String,
