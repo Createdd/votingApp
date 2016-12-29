2a27fdf6406e856c0c2307c5d3eadf5f137a260e
Daniel Deutsch <deudan1010@gmail.com>
2016-12-29 16:55:06 +0100

    add validation in user.js

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,37 @@
 'use strict';
 const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
+let emailRegex = new RegExp('^([a-z0-9-_\.\!\#\$\%\&\'\*\+\/\=\?\^\`\{\|\}\~]+@(?:[a-z0-9\-]+)(?:\.[0-9a-z\-]+)+)$', 'i');
 
+//----------Validations
+let usernameValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3,50],
+    message: 'Name should have between {ARGS[0]} and {ARGS[1]} characters'
+  }),
+  validate({
+    validator: 'isAlphaNumeric',
+    passIfEmpty: false,
+    message: 'Name should only contain alphanumeric characters'
+  })
+];
+let emailValidator = [
+  validate({
+    validator: 'isEmail',
+    message: 'Invalid Email'
+  })
+];
+let passwordValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [6,32],
+    message: 'Password should have between {ARGS[0]} and {ARGS[1]} characters'
+  })
+];
+
+
+//----------User Schema
 var userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -8,18 +39,21 @@ var userSchema = mongoose.Schema({
     unique: true,
     index: {
       unique: true
-    }
+    },
+    validate: usernameValidator
   },
   email: {
     type: String,
     required: true,
     lowercase: true,
-    unique: true
+    unique: true,
+    validate: emailValidator
   },
   password: {
     type: String,
     required: true,
-    select: false
+    select: false,
+    validate: passwordValidator
   }
 });
 let User = mongoose.model('User',userSchema);
