d7ee3a82fa832e32646a302cef8ea1dd4040495c
Daniel Deutsch <deudan1010@gmail.com>
2016-12-29 22:50:59 +0100

    add poll Schema

--- app/models/poll.js
+++ app/models/poll.js
@@ -1,3 +1,18 @@
 'use strict';
 const mongoose = require('mongoose');
 const validate = require('mongoose-validator');
+
+var pollSchema = mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+    validate: questionValidator
+  },
+  choices: {
+    type: Object,
+    required: true,
+    validate: choicesValidator
+  }
+});
+let Poll = mongoose.model('Poll', pollSchema);
+module.exports = Poll;
