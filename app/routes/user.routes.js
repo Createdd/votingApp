d07174bbb2fd80f6674680d4a916b07986941a7e
Daniel Deutsch <deudan1010@gmail.com>
2016-12-29 12:18:32 +0100

    add other methods

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,6 +4,8 @@ const _ = require('underscore');
 
 module.exports = function(express,app) {
   let router = express.Router();
+
+  //----------Find existing users
   router.get('/', (req, res) => {
     User.find({}, (err,users) => {
       if(err) {
@@ -16,10 +18,8 @@ module.exports = function(express,app) {
       }
     });
   });
-  /*router.get('/:id', (req,res) => {
-    let userId = req.params.id;
 
-  });*/
+  //----------Create/save users
   router.post('/', (req,res) => {
     let validAttributes = _.pick(req.body, 'username', 'email', 'password');
     let newUser = new User (validAttributes);
@@ -31,5 +31,62 @@ module.exports = function(express,app) {
       }
     });
   });
+
+  //----------Find existing user with id
+  router.get('/:id', (req,res) => {
+    let userId = req.params.id;
+    User.findById({_id: userId}, (err, user) => {
+      if(err) {
+        res.status(500).send('Error in Request');
+        return console.error(err);
+      } else {
+        if(user){
+          res.json(user);
+        } else {
+          res.status(404).send('User not found');
+        }
+      }
+    });
+  });
+
+  //----------update User
+  router.put('/:id', (req,res) => {
+    let userId = req.params.id;
+    let validAttributes = _.pick(req.body, 'username', 'email', 'password');
+    User.findById({_id: userId}, (err, user) => {
+      if(err) {
+        console.log(err);
+        return res.status(500).send('Error in Request');
+      }
+      if(user){
+        if(validAttributes.username) user.username = validAttributes.username;
+        if(validAttributes.email) user.email = validAttributes.email;
+        if(validAttributes.password) user.password = validAttributes.password;
+      } else {
+        return res.status(404).send('Cannot find User');
+      }
+      user.save((err,user) => {
+        if(err){
+          console.log(err);
+          res.status(500).send('Cannot update user');
+        } else {
+          res.json({success:true, message: 'User updated'});
+        }
+      });
+    });
+  });
+
+  //----------delete user
+  router.delete('/:id', (req,res) => {
+    let userId = req.params.id;
+    User.remove({_id: userId}, err => {
+      if(err) {
+        console.log(err);
+        res.status(500).send('Cannot delete user');
+      }
+      res.send('User deleted');
+    });
+  });
+
   return router;
 };
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ Created by Daniel Deutsch (<kbd>
 </kbd>)
 
 ### FreeCodeCamp Dynamic Web Application Projects:
-# Build A Voting App [![Travis CI](https://travis-ci.org/DDCreationStudios/votingApp.svg?branch=master)](https://travis-ci.org/DDCreationStudios/votingApp) [![Code Climate](https://codeclimate.com/github/DDCreationStudios/votingApp/badges/gpa.svg)](https://codeclimate.com/github/DDCreationStudios/votingApp)[![dependencies Status](https://david-dm.org/DDCreationStudios/votingApp/status.svg)](https://david-dm.org/DDCreationStudios/votingApp)
+# Build A Voting App [![Travis CI](https://travis-ci.org/DDCreationStudios/votingApp.svg?branch=master)](https://travis-ci.org/DDCreationStudios/votingApp) [![Code Climate](https://codeclimate.com/github/DDCreationStudios/votingApp/badges/gpa.svg)](https://codeclimate.com/github/DDCreationStudios/votingApp) [![dependencies Status](https://david-dm.org/DDCreationStudios/votingApp/status.svg)](https://david-dm.org/DDCreationStudios/votingApp)
 
 ## Watch it on Heroku:
 <a href="https://ddcs-votingapp.herokuapp.com/" target="_blank">https://ddcs-votingapp.herokuapp.com/</a>
