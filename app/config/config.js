e18c772aa15a024b6e328b5bb87cc2df93981718
Daniel Deutsch <deudan1010@gmail.com>
2017-01-07 12:30:14 +0100

    add config for hashing secret

---
+++ app/config/config.js
@@ -0,0 +1,4 @@
+'use strict';
+let config = {};
+config.privateKey = "secret";
+module.exports = config;
--- app/middleware/auth-middleware.js
+++ app/middleware/auth-middleware.js
@@ -1,8 +1,20 @@
 'use strict';
-//const config = require('../config/config');
+const config = require('../config/config');
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req,res,next) {
   let token = req.body.token || req.param('token') || req.headers['x-access-token'];
-  
+
+  if(token) {
+    jwt.verify(token, config.privateKey, (err,decoded) => {
+      if(err) {
+        return res.status(401).send('Failed to authenticate token');
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+
+
 };
