949b658c89825d073d75cb6b0093bc81a45965a0
Daniel Deutsch <deudan1010@gmail.com>
2017-01-09 15:51:56 +0100

    set up mlab config

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,12 @@
 'use strict';
 let config = {};
 config.privateKey = "secret";
+config.db = {};
+
+config.db.user = 'voting_management';
+config.db.password = 'votingapp';
+config.db.connectString =
+  'mongodb://' + config.db.user + ':' + config.db.password + '@ds141368.mlab.com:41368/votingapp';
+
+
 module.exports = config;
