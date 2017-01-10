435507ddbe851aa9689283fdaad73494c84c1b2b
Daniel Deutsch <deudan1010@gmail.com>
2017-01-10 12:19:21 +0100

    update webpack for sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './public/components/index.js',
   output: {
@@ -10,14 +12,18 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style-loader', 'cssloader!sass-loader')
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('./public/styles/main.css')
+  ],
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    extensions: ['', '.js', '.jsx', '.scss', '.json']
   }
 };
