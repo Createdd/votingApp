abcd00c06542ce211ce199e308270218a9b0f7ad
Daniel Deutsch <deudan1010@gmail.com>
2017-01-18 11:32:42 +0100

    add signUp form

---
+++ public/src/components/LoginC.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class Signup extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col s4">
+          <h3>Social Media Logins</h3>
+        </div>
+        <div className="col s8">
+          <form className="input-field col s10">
+            <div className="input-field">
+              <input id="name" type="text" class="validate" />
+              <label for="name">Name</label>
+            </div>
+            <div className="input-field">
+              <input id="email" type="text" class="validate" />
+              <label for="email">E-Mail</label>
+            </div>
+            <div className="input-field ">
+              <input id="password" type="password" class="validate" />
+              <label for="password">Password</label>
+            </div>
+            <div className="input-field">
+              <input id="confirmPassword" type="password" class="validate" />
+              <label for="confirmPassword">Confirm Password</label>
+            </div>
+            <button
+              className="btn waves-effect waves-green blue-grey darken-3"
+              type="submit"
+              name="action"
+            >
+              Submit
+              <i className="material-icons right">flight_takeoff</i>
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ public/src/components/WelcomeC.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+export default class Welcome extends React.Component {
+  render () {
+    return (
+      <div>
+
+        <div className="parallax-container">
+           <div className="parallax">
+            <img src="https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="></img>
+          </div>
+        </div>
+
+        <div className="section grey darken-2">
+          <div className="row">
+            <div className="col s12 m4 card-panel hoverable teal">
+              <div className="card blue-grey darken-4 center-align">
+                <div className="card-content teal-text">
+                  <i className="large material-icons">check_circle</i><br></br>
+                  <span className="card-title">Vote And See Results</span>
+                  <p>Everyone can vote, add another option and see the results</p>
+                </div>
+                <div className="card-action">
+                  <a href="#">Check It Out</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col s12 m4 card-panel hoverable teal">
+              <div className="card blue-grey darken-4 center-align">
+                <div className="card-content teal-text">
+                  <i className="large material-icons">open_in_new</i><br></br>
+                  <span className="card-title">Create And Share Polls</span>
+                  <p>Create, edit, delete and share your Poll as authenticated user</p>
+                </div>
+                <div className="card-action">
+                  <a href="#">Log In Now</a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col s12 m4 card-panel hoverable teal">
+              <div className="card blue-grey darken-4 center-align">
+                <div className="card-content teal-text">
+                  <i className="large material-icons">assessment</i><br></br>
+                  <span className="card-title">Databases And Charts</span>
+                  <p>Experience your results with updated charts and Databases</p>
+                </div>
+                <div className="card-action">
+                  <a href="#">Check It Out</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="parallax-container">
+          <div className="parallax">
+            <img src="https://images.unsplash.com/photo-1470074506451-60892faba445?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop="></img>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
