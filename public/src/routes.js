c955d8ab459b746ca9efe65f69f67e797802c911
Daniel Deutsch <deudan1010@gmail.com>
2017-01-22 12:06:00 +0100

    add login route

--- public/src/routes.js
+++ public/src/routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './Components/AppC.js';
 import Header from './Components/HeaderC.js';
 import Footer from './Components/FooterC.js';
 import Welcome from './Components/WelcomeC.js';
 import SignUp from './Components/SignUpC.js';
+import Login from './Components/LoginC.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Welcome} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/login" component={Login} />
   </Route>
 );
