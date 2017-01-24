56989b70e5d41f7c0aeb0bb9a2fbfb9590769538
Daniel Deutsch <deudan1010@gmail.com>
2017-01-24 16:40:35 +0100

    update routes

--- public/src/routes.js
+++ public/src/routes.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import App from './Components/AppC.js';
-import Header from './Components/HeaderC.js';
-import Footer from './Components/FooterC.js';
-import Welcome from './Components/WelcomeC.js';
-import SignUp from './Components/SignUpC.js';
-import Login from './Components/LoginC.js';
-import Lost from './Components/LostC.js';
+import App from './components/AppC.js';
+import Header from './components/HeaderC.js';
+import Footer from './components/FooterC.js';
+import Welcome from './components/WelcomeC.js';
+import SignUp from './components/SignUpC.js';
+import Login from './components/LoginC.js';
+import Lost from './components/LostC.js';
+import AllPolls from './containers/allPolls.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Welcome} />
     <Route path="/signup" component={SignUp} />
     <Route path="/login" component={Login} />
+    <Route path="/polls" component={AllPolls} />
     <Route path="*" component={Lost} />
   </Route>
 );
