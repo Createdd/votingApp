931714fd0f9f1a78c8c12a3957de181ed261e05f
Daniel Deutsch <deudan1010@gmail.com>
2017-01-24 12:43:24 +0100

    update poll choice

--- public/src/components/PollChoiceC.js
+++ public/src/components/PollChoiceC.js
@@ -1,32 +1,26 @@
 import React from 'react';
 
-const pollChoice = (props) => {
+const PollChoice = props => {
   return (
-    <div className="row grey darken-2">
-      <div className="col s8 offset-s2">
-        <form action="#">
-          <input
-            id="choice"
-            type="radio"
-            name="choice"
-            aria-label={'Label for: ' + props.choice}
-            className="with-gap"
-            checked
-          />
-          <label forHtml="choice">
-            <i className="small material-icons prefix teal-text">
-              playlist_add
-            </i>
-          </label>
-          <input
-            type="text"
-            className="validate"
-            value={props.choice}
-            aria-label={'Label for: ' + props.choice}
-            disabled="disabled"
-          />
-        </form>
-      </div>
+    <div>
+      <input
+        id="choice"
+        type="radio"
+        name="choice"
+        aria-label={'Label for: ' + props.choice}
+        className="with-gap"
+        checked
+      />
+      <label forHtml="choice">
+        <i className="small material-icons prefix teal-text">playlist_add</i>
+      </label>
+      <input
+        type="text"
+        className="validate"
+        value={props.choice}
+        aria-label={'Label for: ' + props.choice}
+        disabled="disabled"
+      />
     </div>
   );
 };
