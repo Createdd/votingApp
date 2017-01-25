dc94439b2e60ce0393a14c8858b98433c2f222f1
Daniel Deutsch <deudan1010@gmail.com>
2017-01-25 12:05:13 +0100

    add pollResult route

--- public/src/routes.js
+++ public/src/routes.js
@@ -9,6 +9,7 @@ import SignUp from './components/SignUpC.js';
 import Login from './components/LoginC.js';
 import Lost from './components/LostC.js';
 import AllPolls from './containers/allPolls.js';
+import PollResult from './containers/pollResult.js';
 
 export default (
   <Route path="/" component={App}>
@@ -16,6 +17,7 @@ export default (
     <Route path="/signup" component={SignUp} />
     <Route path="/login" component={Login} />
     <Route path="/polls" component={AllPolls} />
+    <Route path="/chart" component={PollResult} />
     <Route path="*" component={Lost} />
   </Route>
 );
