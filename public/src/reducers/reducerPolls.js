5a35f835fda015fdf3655fd6e966091bfb3b8e64
Daniel Deutsch <deudan1010@gmail.com>
2017-01-25 12:38:22 +0100

    update reducerPolls

--- public/src/reducers/reducerPolls.js
+++ public/src/reducers/reducerPolls.js
@@ -1,11 +1,13 @@
-import { FETCH_ALL_POLLS } from '../actions/index';
+import { FETCH_ALL_POLLS, FETCH_SINGLE_POLL } from '../actions/index';
 
-const INITIAL_STATE = { all: [] };
+const INITIAL_STATE = { all: [], singlePoll: {} };
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case FETCH_ALL_POLLS:
       return { ...state, all: action.payload.data };
+    case FETCH_SINGLE_POLL:
+      return { ...state, singlePoll: action.payload.data };
     default:
       return state;
   }
