5cf29bdf9268dc42581af1b723f4acdd38d29008
Daniel Deutsch <deudan1010@gmail.com>
2017-01-25 12:26:56 +0100

    update fetch action

--- public/src/actions/index.js
+++ public/src/actions/index.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 export const FETCH_ALL_POLLS = 'FETCH_ALL_POLLS';
+export const FETCH_SINGLE_POLL = 'FETCH_SINGLE_POLL';
 const ROOT_URL = 'http://localhost:3000/api';
 
 export function fetchAllPolls() {
   const request = axios.get(`${ROOT_URL}/polls`);
   return { type: FETCH_ALL_POLLS, payload: request };
+}
+
+export function fetchSinglePoll() {
+  const request = axios.get(`${ROOT_URL}/polls/${pollId}`);
+  return { type: FETCH_SINGLE_POLL, payload: request };
 }
