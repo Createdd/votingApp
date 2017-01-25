3c722152e0fe394909481028aedd9cc6e1e23d8a
Daniel Deutsch <deudan1010@gmail.com>
2017-01-25 12:36:46 +0100

    update pollResult with real data

--- public/src/containers/pollResult.js
+++ public/src/containers/pollResult.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSinglePoll } from './actions/index';
+import axios from 'axios';
 
-export default class PollResult extends Component {
+class PollResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          value: 1,
+          label: 'no votes yet',
+          color: 'lightGrey',
+          highlight: 'lightGrey'
+        }
+      ]
+    };
+  }
   componentDidMount() {
-    this.loadPoll();
+    axios.get('http://loclahost:3000/api/polls/' + this.props.id).then(data => {
+      console.log(data);
+      this.loadPoll(data.data);
+    });
   }
 
-  loadPoll() {
-    var data = [
-      { value: 300, color: '#F7464A', highlight: '#FF5A5E', label: 'Red' },
-      { value: 50, color: '#46BFBD', highlight: '#5AD3D1', label: 'Green' },
-      { value: 100, color: '#FDB45C', highlight: '#FFC870', label: 'Yellow' }
-    ];
-    var ctx = document
+  randomColor() {
+    let hexList = '0123456789ABCDEF';
+    let color = '#';
+    let highlight = '#';
+    for (let i = 0; i < 6; i++) {
+      let num = Math.floor(Math.random() * 15);
+      if (num + 2 > 15) {
+        num -= 2;
+      }
+      color += hexList[num];
+      highlight += hexList[num + 2];
+    }
+    return [ color, highlight ];
+  }
+
+  loadPoll(data) {
+    let choices = data.choices;
+    let newdata;
+    if (data.totalVotes === 0) {
+      newdata = [
+        {
+          value: 1,
+          label: 'No votes yet',
+          color: '#cccccc',
+          highlight: '#eeeeee'
+        }
+      ];
+    } else {
+      newdata = Object.keys(choices).map(choice => {
+        // random color each time rendered
+        let colors = this.randomColor();
+        console.log(colors);
+        console.log('value: ', choices[choice], ', label: ', choice);
+        return {
+          value: choices[choice],
+          label: choice,
+          color: colors[0],
+          highlight: colors[1]
+        };
+      });
+    }
+
+    const ctx = document
       .getElementById('result-' + this.props.id)
       .getContext('2d');
     Chart.defaults.global.responsive = true;
-    var myPieChart = new Chart(ctx).Pie(data);
+    const myPieChart = new Chart(ctx).Pie(newdata);
   }
 
   render() {
@@ -26,3 +81,9 @@ export default class PollResult extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { poll: state.polls.singlePoll };
+}
+
+export default connect(mapStateToProps, { fetchSinglePoll })(PollResult);
