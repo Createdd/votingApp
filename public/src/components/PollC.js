c224f2a3d03af1c8760600a47cb13f7ab726d3cc
Daniel Deutsch <deudan1010@gmail.com>
2017-01-25 11:49:32 +0100

    update pollC

--- public/src/components/PollC.js
+++ public/src/components/PollC.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PollChoice from './PollChoiceC.js';
 
 export default class Poll extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showResult: false };
+  }
   renderPollChoices() {
     if (typeof this.props.choices === 'undefined') {
       return null;
     }
-    return this.props.choices.map((choice) => {
-      return <li key={choice}><PollChoice choice={choice} disabled={true} /></li>;
+    return this.props.choices.map(choice => {
+      return (
+        <li key={choice}><PollChoice choice={choice} disabled={true} /></li>
+      );
     });
   }
-  render() {
-    return (
+  renderPoll() {
+    return(
       <div className="row grey darken-2">
         <h1 className="center-align teal-text">Create A Poll</h1>
         <div className="col s8 offset-s2">
@@ -28,12 +34,26 @@ export default class Poll extends React.Component {
               className="btn waves-effect waves-green blue-grey darken-3"
               type="submit"
               name="action"
+              onClick={this.handleVoteClick.bind(this)}
             >
               Create
               <i className="material-icons right">extension</i>
             </button>
           </form>
         </div>
+      </div>
+    );
+  }
+  handleVoteClick() {
+    return (
+      <PollResult id={this.props.id} />
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.question}</h3>
+        {this.state.showResult ? this.renderPollResult() : this.renderPoll() }
       </div>
     );
   }
