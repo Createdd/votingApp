52051fe9df0d0bb1c958cb23488f4bd2f0538c6a
Daniel Deutsch <deudan1010@gmail.com>
2017-01-26 13:22:12 +0100

    update pollC with choice

--- public/src/components/PollC.js
+++ public/src/components/PollC.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PollChoice from './PollChoiceC.js';
+import PollResult from '../containers/pollResult';
+import { connect } from 'react-redux';
+import { updateVotes } from '../actions/index';
 
-export default class Poll extends React.Component {
+class Poll extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showResult: false };
+    this.state = { showResult: false, currentChoice: '' };
+  }
+  handleChecked(choice) {
+    this.setState({ currentChoice: choice });
   }
   renderPollChoices() {
     if (typeof this.props.choices === 'undefined') {
@@ -12,16 +18,33 @@ export default class Poll extends React.Component {
     }
     return this.props.choices.map(choice => {
       return (
-        <li key={choice}><PollChoice choice={choice} disabled={true} /></li>
+        <li key={choice}>
+          <PollChoice
+            choice={choice}
+            disabled={true}
+            checkedOption={this.handleChecked.bind(this)}
+            currentChoice={this.state.currentChoice}
+          />
+        </li>
       );
     });
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    let data = this.state.currentChoice;
+    if (!data || !data.length > 0) {
+      return false;
+    }
+    this.props.updateVotes(this.props.id, data).then(() => {
+      this.setState({ showResult: true });
+    });
+  }
   renderPoll() {
-    return(
+    return (
       <div className="row grey darken-2">
         <h1 className="center-align teal-text">Create A Poll</h1>
         <div className="col s8 offset-s2">
-          <form className="col s10">
+          <form className="col s10" onSubmit={this.handleSubmit.bind(this)}>
             <span className="teal-text">Poll Title: </span>
             <div className="input-field inline">
               <input id="pollTitle" type="text" className="validate" />
@@ -32,9 +55,7 @@ export default class Poll extends React.Component {
             </ul>
             <button
               className="btn waves-effect waves-green blue-grey darken-3"
-              type="submit"
               name="action"
-              onClick={this.handleVoteClick.bind(this)}
             >
               Create
               <i className="material-icons right">extension</i>
@@ -44,17 +65,16 @@ export default class Poll extends React.Component {
       </div>
     );
   }
-  handleVoteClick() {
-    return (
-      <PollResult id={this.props.id} />
-    );
+  renderPollResult() {
+    return <PollResult id={this.props.id} />;
   }
   render() {
     return (
       <div>
         <h3>{this.props.question}</h3>
-        {this.state.showResult ? this.renderPollResult() : this.renderPoll() }
+        {this.state.showResult ? this.renderPollResult() : this.renderPoll()}
       </div>
     );
   }
 }
+export default connect(null, { updateVotes })(Poll);
