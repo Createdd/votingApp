305362c303e567593ba460577191df4a0a77e881
Daniel Deutsch <deudan1010@gmail.com>
2017-05-21 17:17:15 +0200

    reset everything to start again

--- public/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import PollsReducer from './reducerPolls';
-
-const rootReducer = combineReducers({ polls: PollsReducer, form: formReducer });
-
-export default rootReducer;
