a6e709de42d782f06562ba62dc1016ce74e80307
Daniel Deutsch <deudan1010@gmail.com>
2017-05-23 23:15:31 +0200

    add support for travis ci

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
-	target: 'node',
-	externals: [nodeExternals()],
-	entry: {
-		app: './src/app.js'
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].bundle.js',
-		libraryTarget: 'commonjs2'
-	},
-	module: {
-		rules: [
-			{
-				test: /\js$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			}
-		]
-	}
+  target: 'node',
+  externals: [nodeExternals()],
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
