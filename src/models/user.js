91141eda3cee8f2fce523ae1fc4e8c830d915810
Daniel Deutsch <deudan1010@gmail.com>
2017-05-28 12:31:30 +0200

    remove bcrypt since twitter is the only login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,5 @@
 // load the things we need
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
 
 // define the schema for our user model
 const userSchema = mongoose.Schema({
@@ -12,15 +11,4 @@ const userSchema = mongoose.Schema({
   },
 });
 
-// generating a hash
-userSchema.methods.generateHash = function (password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function (password) {
-  return bcrypt.compareSync(password, this.local.password);
-};
-
-// create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
