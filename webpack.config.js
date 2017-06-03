a19b3de0b3327f54868c855d723aff9605caadc8
Daniel Deutsch <deudan1010@gmail.com>
2017-06-03 17:07:49 +0200

    update babel and webpack for react

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       {
         test: /\js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        loader: ['react-hot-loader', 'babel-loader'],
       },
     ],
   },
